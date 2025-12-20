f8c04fa7689dbbd71d2db9a386a9651020d3dec2
andrelimarn <andrelima.rn@gmail.com>
2025-12-20 12:17:26 -0300

    fix: Correction in the robots.txt file.

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,8 +1,7 @@
 import { MetadataRoute } from 'next';
 
 export default function robots(): MetadataRoute.Robots {
-  const baseUrl =
-    process.env.NEXT_PUBLIC_BASE_URL || 'hhttps://shortly-psi-two.vercel.app';
+  const baseUrl = process.env.NEXT_PUBLIC_BASE_URL || 'https://shortlyurl.cc';
 
   return {
     rules: {
